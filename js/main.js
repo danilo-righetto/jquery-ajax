c4a4caf50a9e58285e00e7a0277f84b01d68323a
Danilo <danilonewtrue@gmail.com>
2016-07-28 20:57:01 -0300

    Serialize e SerializeArray

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 /* Arquivo principal JS */
 $(function() {
     $('#AjaxRequest').submit(function() {
+        // outra forma de recuperar todos dados do formulario
+        var form = $(this).serealize();
+
+        // metodo semelhante ao serealize()
+        // porem ele traz os dados no formato de Array
+        // var formArray = $(this).serealizeArray();
+
+        console.log(form); // testando form
+        console.log(formArray); // testando formArray
+
         // usando o Ajax do JQuery
         var request = $.ajax({
             // vamos passar parametros aqui
@@ -8,14 +18,21 @@ $(function() {
             //url - destino da nossa requisicao
             url: "post.php",
             // vamos capturar os dados da nossa requisição
+            /*
             data: {
                 name: $(':input[name=name]').val(),
                 email: $(':input[name=email]').val(),
                 tel: $(':input[name=tel]').val()
             }
+            */
+
+            /* Outra forma e pegar os dados usando data() */
+
+            data: form // ou formArray
         });
 
         // e - variavel que vai receber os dados do request
+        // always() - serve para executar uma ação quando houve sucesso do request
         request.always(function(e) {
             console.log(e);
         });
--- post.php
+++ post.php
@@ -4,6 +4,8 @@
 if ($_GET) {
     // ele para assim que ele mostrar o $_GET
     var_dump($_GET);exit;
+    // caso precisasemos pegar um valor do array
+    // poderiamos pegar assim: $_GET['name']
 }
 
 if ($_POST) {
